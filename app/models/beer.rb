d52bf659453d4817e6d8a45e24d1182e6dd00566
Jesper Kuutti <jesper.kuutti@helsinki.fi>
2018-09-11 18:03:11 +0300

    Viikon 2 tehtävä 6 tehty - arvioiden lkm näkyy

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,12 @@
 class Beer < ApplicationRecord
     belongs_to :brewery
     has_many :ratings
+
+    def average
+        if self.ratings.empty? 
+            return nil 
+        end
+       sum =  self.ratings.inject(0) {|sigma, rating| sigma + rating.score}
+       (1.0* sum) / self.ratings.count
+    end 
 end
