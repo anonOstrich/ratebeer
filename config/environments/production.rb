daeaecff76945288c62f6393defe332fe54b3de0
Jesper Kuutti <jesper.kuutti@helsinki.fi>
2018-10-18 01:14:36 +0300

    Travis testi

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "ratebeer_#{Rails.env}"
+  config.assets.js_compressor = Uglifier.new(harmony: true)
 
   config.action_mailer.perform_caching = false
 
